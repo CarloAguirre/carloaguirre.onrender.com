aec98f4a270cc2b2f1722e4ba9497929627bcf04
Carlo Aguirre <carlo_aguirre@outlook.cl>
2023-10-12 15:14:58 -0300

    img url modified for render.com visibility

--- src/sideBar/SideBar.jsx
+++ src/sideBar/SideBar.jsx
@@ -3,7 +3,7 @@ import './sideBar.scss'
 export const SideBar = () => {
   return (
     <div className='sideBar-container'>
-        <img src="imagen1.png" alt="" />
+        <img src="https://res.cloudinary.com/dezwpnks0/image/upload/v1697134425/Imagen1_eswblp.webp" alt="" />
     </div>
   )
 }
