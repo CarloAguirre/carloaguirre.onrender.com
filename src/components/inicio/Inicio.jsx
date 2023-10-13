55d4df0618357c9cee8237b6ef5eec8710cc7272
Carlo Aguirre <carlo_aguirre@outlook.cl>
2023-10-13 12:15:52 -0300

    cv downloader link

--- src/components/inicio/Inicio.jsx
+++ src/components/inicio/Inicio.jsx
@@ -12,9 +12,17 @@ export const Inicio = () => {
                 <h1>Soy <br />Carlo Aguirre Chacón,</h1>
                 <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum pariatur labore, incidunt minima velit exercitationem, fugit voluptas fugiat ab doloremque nihil beatae sequi culpa assumenda dolorem odio ea repudiandae! Ipsa.</p>
                 <div className='buttons-wrapper'>
-                  <Button className='button-cv'><div className='btn-custom'><p>Descargar Curriculum</p> <span class="material-symbols-outlined ms-2">
-                  download
-                  </span></div></Button>
+                <Button className='button-cv'>
+                  <a href="/C.V_carlo-aguirre.pdf" download="C.V_carlo-aguirre.pdf">
+                    <div className='btn-custom'>
+                      <p>Descargar Curriculum</p> 
+                      <span className="material-symbols-outlined ms-2">
+                        download
+                      </span>
+                    </div>
+                  </a>
+                </Button>
+
                   <Button variant='secondary'><div className='btn-custom'><p>Contactar</p> <span class="material-symbols-outlined ms-2">
                   send
                   </span></div></Button>
