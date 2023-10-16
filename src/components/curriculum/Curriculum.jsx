2c91122ff6c794e778d50a3b907cc00bb2679bcc
Carlo Aguirre <carlo_aguirre@outlook.cl>
2023-10-16 12:43:40 -0300

    curriculum completed and pofile description

--- src/components/curriculum/Curriculum.jsx
+++ src/components/curriculum/Curriculum.jsx
@@ -3,8 +3,9 @@ import './curriculum.scss'
 export const Curriculum = () => {
   return (
     <div className='curriculum-wrapper'>
+        <h1 className='mb-5'>Historial Laboral</h1>
         <div>
-            <h1>Desarrollo y administración</h1>
+            <h3>Desarrollo web y administración</h3>
             <br />
             <br />
             <div>
@@ -24,7 +25,7 @@ export const Curriculum = () => {
             <hr /><br />
         </div>
         <div>
-            <h1>Desarrollador de aplicacion web</h1>
+            <h3>Desarrollador de aplicacion web</h3>
             <br />
             <br />
             <div>
@@ -42,7 +43,7 @@ export const Curriculum = () => {
             <hr /><br />
         </div>
         <div>
-            <h1>Desarrollador de aplicacion web</h1>
+            <h3>Desarrollador de aplicacion web</h3>
             <br />
             <br />
             <div>
@@ -58,6 +59,68 @@ export const Curriculum = () => {
                 <p>-Despliegue de servidor, base de datos y aplicación en hosting.</p>
                 <br />
                 <p>sept. 2022 - abr. 2023 · 8 meses</p>
+            </div><br />
+            <hr /><br />
+        </div>
+        <div>
+            <h3>Creación de contenidos digitales</h3>
+            <br />
+            <br />
+            <div>
+                <h4>Youtube partners · Autónomo</h4>
+                <h5>Creador de contenidos para plataformas digitales en @Permacultural</h5>
+                <br /> 
+                <p>FUNCIONES DEL CARGO:</p>
+                <br />
+                <p>-Desarrollo de concepto y guión </p>
+                <br /> 
+                <p>-Edicion y musicalizacion de videos </p>
+                <br />
+                <p>-Labores agricolas.</p>
+                <br />
+                <p>oct. 2020 - ago. 2021 · 11 meses</p>
+            </div><br />
+            <hr /><br />
+        </div>
+        <div>
+            <h3>Administración y atención de clientes</h3>
+            <br />
+            <br />
+            <div>
+                <h4>Savory</h4>
+                <h5>Administracion de punto de ventas Savory (Vicuña, La Serena)</h5>
+                <br /> 
+                <p>FUNCIONES DEL CARGO:</p>
+                <br />
+                <p>-Implementacion de software 'Punto de venta' </p>
+                <br /> 
+                <p>-Administración fizcal y de personal</p>
+                <br />
+                <p>-Control de stock y labores relacionadas</p>
+                <br />
+                <p>-Atención esporadica de clientes</p>
+                <br />
+                <p>mar. 2018 - nov. 2019 · 1 año y 8 meses</p>
+            </div><br />
+            <hr /><br />
+        </div>
+        <div>
+            <h3>Atención de clientes</h3>
+            <br />
+            <br />
+            <div>
+                <h4>Local 'Llaves y Videos'</h4>
+                <h5>Atencion de clientes en punto de ventas (Supermercado Unimark, AV. Angamos, Antofagasta)</h5>
+                <br /> 
+                <p>FUNCIONES DEL CARGO:</p>
+                <br />
+                <p>-Atencion de clientes</p>
+                <br /> 
+                <p>-Cotrol de videoteca (DVD's)</p>
+                <br />
+                <p>-Manejo de equipos para la confección de llaves</p>
+                <br />
+                <p>jun 2013 - sep. 2013 · 4 meses</p>
             </div><br />
             <hr /><br />
         </div>
