96c063ca29f668776c5f0e267af64c8ae6bfd6d9
Carlo Aguirre <carlo_aguirre@outlook.cl>
2024-05-11 18:08:14 -0400

    lighthouse optimizations

--- src/components/inicio/Inicio.jsx
+++ src/components/inicio/Inicio.jsx
@@ -8,7 +8,7 @@ export const Inicio = () => {
   return (
     <div>
         <div className='profile-container'>
-            <img src="yo.png" alt="" />
+            <img src="https://res.cloudinary.com/dezwpnks0/image/upload/v1715463187/yo_khrgm7.webp" alt="mi imagen" className='mi-imagen'/>
             <div className='profile-desc'>
                 <h1>Soy <br />Carlo Aguirre Chacón,</h1>
                 <p>Programador web con experiencia en el desarrollo de sitios altamente escalables. Poseo una sólida base en JavaScript/TypeScript y sus diferentes frameworks tanto de frontend como backend. Mi stack se basa principalmente en React.js/Angular + Node.js + PostgreSQL/MongoDB. </p>
